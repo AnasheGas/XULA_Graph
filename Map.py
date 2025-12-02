8ecb19e9fbf6e3ece2ef295ee7a909660b4c380d
Anashe Gaseller <anashegas@gmail.com>
2025-12-01 18:37:37 -0600

    add Euclidean

--- Map.py
+++ Map.py
@@ -6,8 +6,6 @@
 # at least 20 edges between those vertexes.  
 # Each building has a name and accurate gps coordinates.
 
-def main():
-   print(f"\n{'='*20} Welcome to Anashe's XULA Graph app! {'='*20}\n")
 
 # List of locations with their Name, latitude and longitude
 import math
@@ -47,7 +45,6 @@ graph = {
 }
 
 # TODO Finsih eclidian and halversine distance functions
-<<<<<<< HEAD
 def find_euclidean_distance(loc1, loc2):
    lat1 = loc1[1]
    lon1 = loc1[2] 
@@ -81,32 +78,23 @@ def find_haversine_distance(loc1, loc2):
 
 
    
-=======
-def find_euclidean_distance(loc1: tuple[str, float, float], loc2: tuple[str, float, float]):
-  pass
->>>>>>> 4b3f672ae55d6b27c7c7f9d8d13f708203b52a4f
 
 
 
 #Find all adjecent neighbors of a given location
 # graph[key].len 
-def find_neighbors(location: tuple[str, float, float]):
+def find_neighbors(location: tuple):
    pass
 
 #need visted list, set
 # use BFS
-def find_neighbors_within(num_edges: int, location: tuple[str, float, float]):
+def find_neighbors_within(num_edges: int, location: tuple):
    pass
 
-<<<<<<< HEAD
 def main():
    print("Graph Locations and their connections:")
    print(find_euclidean_distance(locations[0], locations[1]))
    print(find_haversine_distance(locations[0], locations[1]))
 
 if __name__ == "__main__":
-=======
-
-if __name__ == '__main__':
->>>>>>> 4b3f672ae55d6b27c7c7f9d8d13f708203b52a4f
    main()
