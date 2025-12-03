0bdf9b6b01826acb015e3ee58039160fa18a230b
Andrea Edwards <aedwards@xula.edu>
2025-12-02 20:37:30 -0600

    Add type hinting and TODOs to create a Building class.

--- Map.py
+++ Map.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 #Add New Orleans (Latitude 29.9547, Longitude -90.0751), 
 # Abuja (9.084576, 7.483333) and 
 # Harare (-17.824858, 31.053028).
@@ -8,8 +11,9 @@
 
 
 # List of locations with their Name, latitude and longitude
-import math
 
+
+# TODO This should be in a Buildings class.
 locations = [
     
     ("New Orleans" , 29.9547, -90.0751),
@@ -28,6 +32,7 @@ locations = [
     ("Music Building", 29.9640, -90.1079)
 ]
 
+# TODO This should be in a Buildings class.
 graph = {
    locations[0]: [locations[1]],
    locations[1]: [locations[2], locations[4]],
@@ -44,16 +49,16 @@ graph = {
    locations[12]: [locations[8], locations[1]]
 }
 
-# TODO Finsih eclidian and halversine distance functions
-def find_euclidean_distance(loc1, loc2):
+# TODO This should be a method in your Buildings class.
+def find_euclidean_distance(loc1: tuple[str, float, float], loc2: tuple[str, float, float]):
    lat1 = loc1[1]
    lon1 = loc1[2] 
    lat2 = loc2[1]
    lon2 = loc2[2]
    return math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2) 
 
-
-def find_haversine_distance(loc1, loc2):
+# TODO This should be a method in your Buildings class.
+def find_haversine_distance(loc1: tuple[str, float, float], loc2: tuple[str, float, float]):
    lat1 = loc1[1]
    lon1 = loc1[2]
    lat2 = loc2[1]
@@ -74,21 +79,16 @@ def find_haversine_distance(loc1, loc2):
    R = 6371
    return R * c
 
-
-   
-
-
-
-#Find all adjecent neighbors of a given location
-# graph[key].len 
-def find_neighbors(location: tuple):
+# TODO This should be a method in your Buildings class.
+def find_neighbors(location: tuple[str, float, float]):
+   '''Return all adjacent neighbors of the given location.'''
    return graph.get(location, [])
 
-#need visted list, set
-# use BFS
-def find_neighbors_within(num_edges: int, location: tuple):
-   num_edges = 2
-   ans = set()
+# TODO This should be a method in your Buildings class.
+# TODO Make this code work for every value of num_edges.
+def find_neighbors_within(num_edges: int, location: tuple[str, float, float]) -> set[tuple[str, float, float]]:
+   '''Return all neighbors with 2 edges of the given location.'''
+   ans: set[tuple[str, float, float]] = set()
    neighbors = find_neighbors(location)
    ans.update(set(neighbors))
    for neighbor in neighbors:
@@ -96,8 +96,9 @@ def find_neighbors_within(num_edges: int, location: tuple):
   
    return ans
 
+# TODO This should be a method in your Buildings class.
 # Build a weighted graph where each neighbor has a distance
-weighted_graph = {}
+weighted_graph: dict[tuple[str, float, float], list[tuple[tuple[str, float, float], float]]] = {}
 
 for node, neighbors in graph.items():
     weighted_graph[node] = []
@@ -105,17 +106,17 @@ for node, neighbors in graph.items():
         distance = find_euclidean_distance(node, neighbor)
         weighted_graph[node].append((neighbor, distance))
 
-
-def dijkstra_shortest_path_euclidean(start_loc: tuple, end_loc: tuple):
+# TODO This should be a method in your Buildings class.
+def dijkstra_shortest_path_euclidean(start_loc: tuple[str, float, float], end_loc: tuple[str, float, float]) -> tuple[list[str], float]:
     # Initialize all nodes as unvisited with infinite distance
-    unvisited = {loc: float('inf') for loc in graph}
+    unvisited: dict[tuple[str, float, float], float] = {loc: float('inf') for loc in graph}
     unvisited[start_loc] = 0
-    visited = {}
-    previous_nodes = {}
+    visited: dict[tuple[str, float, float], float] = {}
+    previous_nodes: dict[tuple[str, float, float], tuple[str, float, float]] = {}
 
     while unvisited:
-        current_loc = min(unvisited, key=unvisited.get)
-        current_distance = unvisited[current_loc]
+        current_loc: tuple[str, float, float] = min(unvisited, key=lambda k: unvisited[k])
+        current_distance: float = unvisited[current_loc]
 
         # Stop if remaining nodes are unreachable
         if current_distance == float('inf'):
@@ -123,12 +124,20 @@ def dijkstra_shortest_path_euclidean(start_loc: tuple, end_loc: tuple):
 
         # Stop if we've reached the target
         if current_loc == end_loc:
+            # Finalize the end node distance before breaking
+            visited[current_loc] = current_distance
+            unvisited.pop(current_loc)
             break
 
         # Explore neighbors
         for neighbor in graph.get(current_loc, []):
+            # Skip neighbors already finalized
+            if neighbor in visited:
+                continue
             new_distance = current_distance + find_euclidean_distance(current_loc, neighbor)
-            if new_distance < unvisited.get(neighbor, float('inf')):
+            # Only relax if neighbor is still unvisited and new distance is shorter
+            old_distance = unvisited.get(neighbor, float('inf'))
+            if new_distance < old_distance:
                 unvisited[neighbor] = new_distance
                 previous_nodes[neighbor] = current_loc
 
@@ -136,15 +145,23 @@ def dijkstra_shortest_path_euclidean(start_loc: tuple, end_loc: tuple):
         visited[current_loc] = current_distance
         unvisited.pop(current_loc)
 
-    # Reconstruct the shortest path
-    path = []
+    # Reconstruct the shortest path with cycle safety
+    path: list[tuple[str, float, float]] = []
     node = end_loc
-    while node in previous_nodes:
-        path.insert(0, node)
-        node = previous_nodes[node]
+    seen: set[tuple[str, float, float]] = set()
 
-    if path:
-        path.insert(0, start_loc)
+    # Only attempt reconstruction if the end node has a finite distance
+    if visited.get(end_loc, float('inf')) != float('inf') or end_loc in previous_nodes:
+        while node in previous_nodes and node not in seen:
+            path.insert(0, node)
+            seen.add(node)
+            node = previous_nodes[node]
+
+        if node == start_loc:
+            path.insert(0, start_loc)
+        elif path and path[0] != start_loc:
+            # If we didn't reach the start, the path is incomplete; clear it
+            path = []
 
     # Return the path as names and total Euclidean distance
     path_names = [loc[0] for loc in path]
@@ -162,14 +179,13 @@ def dijkstra_shortest_path_euclidean(start_loc: tuple, end_loc: tuple):
 def main():
    
    
-   start = locations[0]  # "Outreach Center"
-   end = locations[2]   # "Convocation Center"
+   start_building = locations[random.randint(0, len(locations))]
+   end_building = locations[random.randint(0, len(locations))]
+   print(f"The randomly selected buildings are {start_building} and {end_building}.")
 
-   path, distance = dijkstra_shortest_path_euclidean(start, end)
+   path, distance = dijkstra_shortest_path_euclidean(start_building, end_building)
+   print(f"Shortest path is {" -> ".join(path)} and has the Euclidean distance {distance} km.")
 
-   print("Shortest path:")
-   print(" -> ".join(path))
-   print("Total Euclidean distance:", distance)
 
 if __name__ == "__main__":
    main()
